cb9ca52f9eb66e79e730776ac335cb2fad9273fc
JeBread <108921478+JeBread@users.noreply.github.com>
2024-05-01 22:17:22 +0900

    [Gold V] Title: 동전, Time: 152 ms, Memory: 112444 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+def knapsack(n, coins, cost):
+    DP = [[0] * (cost + 1) for _ in range(n+1)]
+    DP[0][0] = 1
+    for i in range(1, n+1):
+        coin = coins[i]
+        for j in range(0, cost+1):
+            cnt = j//coin
+            for k in range(0,coin*cnt +1,coin):
+                DP[i][j] += DP[i-1][j-k]
+    print(DP[n][cost])
+
+for _ in range(t):
+    n = int(input())
+    coins = [0] + list(map(int, input().split()))
+    cost = int(input())
+    knapsack(n, coins, cost)
