08cf98528d29f1aac727edc3785a1b0a4253d021
JeBread <108921478+JeBread@users.noreply.github.com>
2024-05-05 16:25:40 +0900

    [level 0] Title: k의 개수, Time: 21.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -5,3 +5,10 @@ def solution(i, j, k):
             if str(k) == y:
                 ans += 1
     return ans
+
+
+# def solution(i, j, k):
+#     answer = 0
+#     for n in range(i, j + 1):
+#         answer += str(n).count(str(k))
+#     return answer
