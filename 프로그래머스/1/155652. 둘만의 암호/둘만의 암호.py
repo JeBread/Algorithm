2f529304b23daf4e67a0105a79716269c80f0c46
JeBread <108921478+JeBread@users.noreply.github.com>
2024-08-10 21:53:26 +0900

    [level 1] Title: 둘만의 암호, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,7 +1,6 @@
 def solution(s, skip, index):
     ans = ''
 #   ord(a) = 97 ~ ord(z) = 122.  ASCII code
-    skip_set = set(skip)  
     
     for char in s:
         cnt = 0
@@ -9,7 +8,7 @@ def solution(s, skip, index):
             char = chr(ord(char) + 1)
             if char > 'z':  
                 char = 'a'
-            if char not in skip_set:
+            if char not in skip:
                 cnt += 1
         
         ans += char
