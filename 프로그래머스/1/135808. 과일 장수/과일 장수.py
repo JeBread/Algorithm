83f36b7a34f6f6841264dfc9922e9109ae981cf5
JeBread <108921478+JeBread@users.noreply.github.com>
2024-08-11 12:41:14 +0900

    [level 1] Title: 과일 장수, Time: 163.09 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,8 @@
 def solution(k, m, score):
     res = 0
     score.sort(reverse=True)
-    box = len(score) // m
-    if box == 0:
-        return res
-    else:
-        for i in range(0, len(score), m):
-            if (i + m) > len(score):
-                return res
-            res += min(score[i:i+m]) * m
-        return res
+    for i in range(0, len(score), m):
+        if (i + m) > len(score):
+            return res
+        res += min(score[i:i+m]) * m
+    return res
